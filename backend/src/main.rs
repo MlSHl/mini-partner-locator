52169b640c0cee9cd25cf421c4164ef252ea4864
Mlshl <mishi.beraia@gmail.com>
2025-07-27 01:54:37 +0400

    Backend: basic rust actrix webserver

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/hello")]
+async fn greet() -> impl Responder {
+    format!("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 8080;
+    println!("Starting server on port: {port}");
+
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", port))?
+        .workers(2)
+        .run()
+        .await
 }
