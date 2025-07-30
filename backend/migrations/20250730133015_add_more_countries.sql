7d3715b12126f50e6c2746627eb7a03712bf49f5
Mlshl <mishi.beraia@gmail.com>
2025-07-30 17:33:28 +0400

    Backend: added sql migratin script for more countries

---
+++ backend/migrations/20250730133015_add_more_countries.sql
@@ -0,0 +1,20 @@
+INSERT INTO countries (name, region) VALUES
+  -- APAC
+  ('Japan', 'APAC'),
+  ('South Korea', 'APAC'),
+  ('Australia', 'APAC'),
+
+  -- LATAM
+  ('Brazil', 'LATAM'),
+  ('Mexico', 'LATAM'),
+  ('Argentina', 'LATAM'),
+  ('Chile', 'LATAM'),
+  ('Colombia', 'LATAM'),
+
+  -- NORAM
+  ('Canada', 'NORAM'),
+
+  -- EMEA
+  ('Italy', 'EMEA'),
+  ('United Kingdom', 'EMEA');
+
